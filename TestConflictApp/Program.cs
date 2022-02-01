40723d8d7b3e323b0080573f98d254ec374b7319
Murat Turan <mturanproje@gmail.com>
2022-02-02 00:36:14 +0300

    Empty lines removed

--- TestConflictApp/Program.cs
+++ TestConflictApp/Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World People!");
-
-
-Console.ReadKey();
-Console.ReadKey();
+Console.ReadKey();Console.ReadKey();
