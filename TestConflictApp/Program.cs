367b6b169a6f96fd5064253e4b78285894e127de
Murat Turan <mturanproje@gmail.com>
2022-02-02 00:58:17 +0300

    Added new GetHelloWorld method to program file

--- TestConflictApp/Program.cs
+++ TestConflictApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World People!");
+using TestConflictApp;
+
+Console.WriteLine(NewClass.GetHelloWorld());
 Console.ReadLine();
 Console.ReadKey();
