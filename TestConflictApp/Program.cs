4a113f1b0a40c1c17d3c264c0215d2f9205aa840
Murat T <46825409+mturan07@users.noreply.github.com>
2022-02-02 01:33:26 +0300

    Added middle comment

--- TestConflictApp/Program.cs
+++ TestConflictApp/Program.cs
@@ -2,6 +2,6 @@ using TestConflictApp;
 // This message comes from newclass file
 Console.WriteLine(NewClass.GetHelloWorld());
 Console.WriteLine("Everything will be okay!");
-
+//This is the middle comment
 Console.ReadKey();
 Console.ReadLine();
