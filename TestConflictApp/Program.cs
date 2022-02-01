4b064cd04700e32143d85c4da5c462dca7a9500a
Murat Turan <mturanproje@gmail.com>
2022-02-02 01:02:30 +0300

    test committed

--- TestConflictApp/Program.cs
+++ TestConflictApp/Program.cs
@@ -1,3 +1,6 @@
-Console.WriteLine("Hello, World People!");
+// See https://aka.ms/new-console-template for more information
+using TestConflictApp;
+
+Console.WriteLine(NewClass.GetHelloWorld());
 Console.ReadLine();
 Console.ReadKey();
