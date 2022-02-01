c48246a773048df90daf4622a2fc17438a60809f
Murat Turan <mturanproje@gmail.com>
2022-02-02 00:48:08 +0300

    New class file added

---
+++ TestConflictApp/NewClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConflictApp
+{
+    public static class NewClass
+    {
+        public static string GetHelloWorld()
+        {
+            return "Hello, world!";
+        }
+    }
+}
