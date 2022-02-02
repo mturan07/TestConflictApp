3f3d81c4c2346e685eeb98e1d0d163d3445451a6
Murat Turan <mturanproje@gmail.com>
2022-02-02 07:04:03 +0300

    Add name changed

--- TestConflictApp/Program.cs
+++ TestConflictApp/Program.cs
@@ -3,7 +3,7 @@ using TestConflictApp;
 Console.WriteLine(NewClass.GetHelloWorld("Hello, world"));
 Console.WriteLine("Everything will be okay!");
 // This is the second comment of the code
-Console.WriteLine($"3 + 4 is {0}", Calc.AddTwoNumber(3, 4));
+Console.WriteLine($"3 + 4 is {Calc.Add(3, 4)}");
 Console.ReadKey();
 Console.ReadLine();
 // This is the last line of the code
