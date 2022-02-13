efcfb62f4e1798eb694cb291d640032c4e2ca89e
Murat Turan <mturanproje@gmail.com>
2022-02-13 07:43:38 +0300

    Initial migration created

--- TestWebApi/Startup.cs
+++ TestWebApi/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestWebApi.Models;
 
 namespace TestWebApi
 {
@@ -26,7 +28,10 @@ namespace TestWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<ApplicationDbContext>(o =>
+            {
+                o.UseSqlServer(Configuration.GetConnectionString("DefaultConn"));
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
