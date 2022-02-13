47ecee815de04141a0a804ba604436a56d39d18f
Murat Turan <mturanproje@gmail.com>
2022-02-13 07:35:17 +0300

    New WebAPI project and Customer class added

---
+++ TestWebApi/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApi.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
